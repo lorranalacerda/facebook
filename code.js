ca59cc82cbf10b9619f912969f57ded246a90eb9
Lorrana Lacerda <146893895+lorranalacerda@users.noreply.github.com>
2024-10-17 10:54:25 -0300

    code.js

--- code.js
+++ code.js
@@ -42,6 +42,39 @@ function post() {
         feed.removeChild(postElement);
     };
     postElement.appendChild(removebotao);
+
+    // reações
+    const reações = document.createElement('div');
+    reações.className = 'reações';
+    reações.innerHTML = `
+    
+        <span class="reaction like" data-reaction="Curtir">&#128077</span>
+        <span class="reaction love" data-reaction="Amei">&#128420</span>
+        <span class="reaction haha" data-reaction="Haha">&#128513</span>
+        <span class="reaction wow" data-reaction="Uau">&#128558</span>
+        <span class="reaction sad" data-reaction="Triste">&#128546</span>
+        <span class="reaction angry" data-reaction="Grr">&#128548</span>
+    `;
+    postElement.appendChild(reações); // Adiciona a barra de reações ao post
+
+    // Adiciona o espaço para exibir a reação selecionada
+    const selectedReaction = document.createElement('div');
+    selectedReaction.className = 'selected-reaction';
+    selectedReaction.innerText = 'Selecione uma reação!'; // Mensagem padrão
+    postElement.appendChild(selectedReaction);
+
+    // Adiciona a funcionalidade de reações
+    const reactions = reações.querySelectorAll('.reaction');
+    reactions.forEach(reaction => {
+        reaction.addEventListener('click', function() {
+            reactions.forEach(r => r.classList.remove('selected')); // Remove a seleção das outras reações
+            this.classList.add('selected'); // Destaca a reação clicada
+            const reactionName = this.getAttribute('data-reaction');
+            selectedReaction.innerText = `Você reagiu com: ${reactionName}`; // Mostra a reação selecionada
+        });
+    });
+
+
  
     feed.prepend(postElement);
     document.getElementById('postContent').value = '';
